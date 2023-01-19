24da727087bb0f76474cfcb94099442689504bda
Michele Ferrari <Michele@0m3x7.onmicrosoft.com>
2023-01-19 17:31:51 +0100

    <Aggiunta del bit di parità verticale>

--- bit_parita/Program.cs
+++ bit_parita/Program.cs
@@ -16,53 +16,102 @@ namespace bit_parita
             int[] bin = new int[8];
             for (int i = 0; i < 7; i++)
             {
-                int resto = decimale % 2; ;
+                int resto = decimale % 2;
                 bin[6 - i] = resto;
                 decimale /= 2;
             }
             return bin;
         }
-        public static int Parity(int[] binario)
+        public static void HParity(ref int[,] matrice, int[] binario, int index)
         {
             int somma = 0;
             for (int i = 0; i < 7; i++)
             {
+                matrice[i, index] = binario[i];
                 somma += binario[i];
             }
             if (somma%2 == 0)
             {
-                return 1;
+                matrice[7, index] = 1;
             }
             else
             {
-                return 0;
+                matrice[7, index] = 0;
             }
         }
-        public static void Visual(int[] binario, int bitparita)
+        public static void VParity(ref int[,] matrice, int index)
         {
+            int somma = 0;
             for (int i = 0; i < 7; i++)
             {
-                Console.Write(binario[i]);
+                somma += matrice[index, i];
             }
-            Console.Write(bitparita);
+            if (somma % 2 == 0)
+            {
+                matrice[index, 7] = 1;
+            }
+            else
+            {
+                matrice[index, 7] = 0;
+            }
+        }
+        public static void Visual(int[,] matrice)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < 8; k++)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    if (k != 7)
+                    {
+                        if (i == 7)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                    }
+                    else
+                    {
+                        if (i == 7)
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                        }
+                    }
+                    Console.Write($"{matrice[i, k]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.ForegroundColor = ConsoleColor.White;
         }
         static void Main(string[] args)
         {
-            int[,] mat = new int[7,7];
+            //inserisci un carattere
+            int[,] mat1 = new int[8,8];
+            char[] inchar = new char[7];
             for (int i = 0; i < 7; i++)
             {
-                //inserisci un carattere
                 Console.WriteLine("Inserire carattere: ");
-                char inchar = (char)Console.Read();
+                inchar[i] = Convert.ToChar(Console.ReadLine());
                 //calcola il codice ascii di un carattere (funz)
-                int dec = ASCII(inchar);
+                int dec = ASCII(inchar[i]);
                 //conversione in binario (funz)
                 int[] bin = Binary(dec);
                 //calcolo il bit di parità (funz)
-                int p = Parity(bin);
-                //visualizzo
-                Visual(bin, p);
+                HParity(ref mat1, bin, i);
             }
+            for (int i = 0; i < 7;i++)
+            {
+                VParity(ref mat1, i);
+            }
+            //visualizzo
+            Visual(mat1);
         }
     }
 }
